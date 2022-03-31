ca6c591e1a97a3159c6729a97347f37b2914b3fb
EsraWameed <esra.wameed@yahoo.com>
2022-03-31 11:29:01 -0700

    directed input into a diff working route through app.get and app.post

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var items=["Study EJS and it's use","Look at GitHub","drink tea and some more tea"];
+let items=["Study EJS and it's use","Look at GitHub","drink tea and some more tea"];
 const app = express();
+let workItems=[];
 app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
 app.get("/", function(req,res){
-
-var today = new Date();
-
-var options ={
+let today = new Date();
+let options ={
   weekday:"long",
   day:"numeric",
   month:"long"
 };
-var day= today.toLocaleDateString("en-US", options);
-
-
-res.render("list", {kindOfDay: day, newListItems: items});
+let day= today.toLocaleDateString("en-US", options);
+res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/",function(req,res){
-  var item= req.body.newItem;
-  items.push(item),
-
-res.redirect("/");
+    let item = req.body.newItem;
+  if (req.body.list==="Work List"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else{
+    items.push(item);
+    res.redirect("/");
+  }
 
 });
+
+app.get("/work", function(req,res){
+  res.render("list",{listTitle: "Work List", newListItems:workItems });
+});
+
 
 app.listen(3000, function(){
   console.log("server started on port 3000");
